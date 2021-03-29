fc36c44f2825418ce09659aaf03a740a180b8876
Isak Lisoff <ilisoff@gmail.com>
2021-03-29 09:22:02 -0700

    Initial commit

---
+++ push-worker.js
@@ -0,0 +1,10 @@
+// 86acbd31cd7c09cf30acb66d2fbedc91daa48b86:1616737908.4154015
+importScripts('https://aswpsdkus.com/notify/v1/ua-sdk.min.js')
+uaSetup.worker(self, {
+  defaultIcon: 'http://isakl.surge.sh/',
+  defaultTitle: 'Isak Lisoff',
+  defaultActionURL: 'http://isakl.surge.sh/',
+  appKey: 'UhfRQC1FTHaHzh7wfo6J7w',
+  token: 'MTpVaGZSUUMxRlRIYUh6aDd3Zm82Sjd3OmJVM3lZNElLSFozd2dOZ1BVNWZ5NVZhNVAyVWJTUEhKNWZMTXVQcEhiRWM',
+  vapidPublicKey: 'BMYA2dqg7ZzdvELDV_uYiyhsIt3ngT3g4Pl1brX0dRTL55Ei4T4cNUIE8eclcDRRSgYdufqwH3U8Kio7Jbm-CdU='
+})
